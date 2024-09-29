a6c9b5240e50efaf07e1b2525c946be9249b79f2
Felipe Bevilacqua <felipe.bevilacqua@maps.com.br>
2024-09-29 14:58:17 -0300

    Correção pagina inicial 2.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,81 @@
+import styles from "../styles/index.css"
+
 function Home() {
-  const index = require("../models/html/index.js");
-  return index.html;
+  return(
+    <html lang="pt-BR">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Felipe Alves Bevilacqua</title>
+      </head>
+      <body class={styles.body}>
+        <header class={styles.header}>
+          <h1>Felipe Alves Bevilacqua</h1>
+        </header>
+        <nav class={styles.nav}>
+          <a class={styles.nav.a} href="#sobre">Sobre</a>
+          <a class={styles.nav.a} href="#portfolio">Portfólio</a>
+          <a class={styles.nav.a} href="#blog">Blog</a>
+          <a class={styles.nav.a} href="#contato">Contato</a>
+        </nav>
+        <section class={styles.section} id="sobre">
+          <div class={styles.container}>
+            <div class="card sobre">
+              <div class="row">
+                <div class="col">
+                  <img
+                    src="../assets/minha_foto.jpeg"
+                    alt="Foto de Felipe Alves Bevilacqua"
+                  />
+                </div>
+
+                <div class="col">
+                  <h2>Sobre</h2>
+                  <p>
+                    Sou engenheiro de software com experiência Full Stack
+                    trabalhando com as linguagens Java, Javascript, Groovy,
+                    Typescript, HTML e HQL. Também utilizo as aplicações e
+                    metodologias IntelliJ, Maven, Springframework, Hibernate, Git,
+                    TDD, SOLID, Linux, PostgreSQL, Kanban, Jenkins, Docker, Angular
+                    e AWS. Trabalho na MAPS SA desde novembro de 2021 no
+                    desenvolvimento de funcionalidades referentes ao software de
+                    precificação de ativos financeiros e deploy de aplicações.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class={styles.section} id="portfolio">
+          <div class={styles.container}>
+            <div class="card">
+              <h2>Portfólio</h2>
+              <p>Em Construção.</p>
+            </div>
+          </div>
+        </section>
+        <section class={styles.section} id="blog">
+          <div class={styles.container}>
+            <div class="card">
+              <h2>Blog</h2>
+              <p>Em Construção.</p>
+            </div>
+          </div>
+        </section>
+        <section class={styles.section} id="contato">
+          <div class={styles.container}>
+            <div class="card">
+              <h2>Contato</h2>
+              <p>WhatsApp: +55 19 9 8315-8939.</p>
+            </div>
+          </div>
+        </section>
+        <footer class={styles.footer}>
+          <p>&copy; 2024 Felipe Alves Bevilacqua</p>
+        </footer>
+      </body>
+    </html>
+  );
 }
 
 export default Home;
