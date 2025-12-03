a1009cedd372b901882182daf6cac4dfeeff566c
Felipe Bevilacqua <felipe.bevilacqua@maps.com.br>
2025-12-03 17:08:48 -0300

    [feat]: Força css para header menu e footer.

--- components/sections/sobre/sobre.js
+++ components/sections/sobre/sobre.js
@@ -19,16 +19,25 @@ const Sobre = () => {
             
             <div className="col col-auto">
               <h2>Sobre</h2>
+              
               <p>
-                -&gt; Sou engenheiro de software com experiência Full Stack
-                trabalhando com as linguagens Java, Javascript, Groovy,
-                Typescript, HTML e HQL. Também utilizo as aplicações e
-                metodologias IntelliJ, Maven, Springframework, Hibernate,
-                Git, TDD, SOLID, Linux, PostgreSQL, Kanban, Jenkins, Docker,
-                Angular e AWS. Trabalho na MAPS SA desde novembro de 2021 no
-                desenvolvimento de funcionalidades referentes ao software de
-                precificação de ativos financeiros e deploy de aplicações.
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-right" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708"/>
+                  <path fill-rule="evenodd" d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708"/>
+                </svg> 
+
+                  <span>
+                    &ensp; Sou engenheiro de software com experiência Full Stack
+                    trabalhando com as linguagens Java, Javascript, Groovy,
+                    Typescript, HTML e HQL. Também utilizo as aplicações e
+                    metodologias IntelliJ, Maven, Springframework, Hibernate,
+                    Git, TDD, SOLID, Linux, PostgreSQL, Kanban, Jenkins, Docker,
+                    Angular e AWS. Trabalho na MAPS SA desde novembro de 2021 no
+                    desenvolvimento de funcionalidades referentes ao software de
+                    precificação de ativos financeiros e deploy de aplicações.
+                  </span>      
               </p>
+
             </div>
           </div>
         </div>
